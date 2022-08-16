a0c94863f7f19c874e41f0ea697070384259cb61
Sam Parry <67956822+xmenbteam@users.noreply.github.com>
2022-08-16 08:38:29 +0100

    Solution

--- election-api-javascript/src/resultsController.js
+++ election-api-javascript/src/resultsController.js
@@ -1,22 +1,56 @@
-const { default: resultStore } = require('./resultsService');
+const { default: resultStore } = require("./resultsService");
 
 const store = resultStore();
 
 function getResult(id) {
-    return store.getResult(id);
+  return store.getResult(id);
 }
 
 function newResult(result) {
-    store.newResult(result);
-    return {};
+  store.newResult(result);
+  return {};
 }
 
 function reset() {
-    store.reset();
+  store.reset();
 }
 
 function scoreboard() {
-    //  Left blank for you to fill in
+  const storeResults = store.getAll();
+
+  // id, name, seqNo, partyResults
+  const result = storeResults.reduce(
+    (obj, { partyResults }) => {
+      obj.sum++;
+      let winner = "";
+      let winnerShare = 0;
+
+      partyResults.forEach(({ party, share, votes }) => {
+        if (!obj.hasOwnProperty(party)) obj[party] = { seats: 0, voteShare: 0 };
+
+        obj[party].voteShare += votes;
+
+        if (share > winnerShare) {
+          winner = party;
+          winnerShare = share;
+        }
+      });
+
+      obj[winner].seats++;
+
+      return obj;
+    },
+    {
+      winner: null,
+      sum: 0,
+    }
+  );
+
+  for (let party in result) {
+    if (result[party]?.seats >= 325) result.winner = party;
+  }
+
+  return result;
 }
 
 module.exports = { getResult, newResult, reset, scoreboard };
--- election-api-javascript/test/scoreboard.spec.js
+++ election-api-javascript/test/scoreboard.spec.js
@@ -1,80 +1,106 @@
-const fs = require('fs');
-const request = require('supertest');
-const { default: expressServer, resetScores } = require('../src/server');
+const fs = require("fs");
+const request = require("supertest");
+const { default: expressServer, resetScores } = require("../src/server");
 
-const resultsSamplesPath = './test/resources/sample-election-results';
+const resultsSamplesPath = "./test/resources/sample-election-results";
 
 function loadAndPostResultFile(server, num) {
-    const fileNumber = new String(parseInt(num, 10)).padStart(3, '0');
-    const result = fs.readFileSync(`${resultsSamplesPath}/result${fileNumber}.json`);
-    return server.post('/result').send(JSON.parse(result));
+  const fileNumber = new String(parseInt(num, 10)).padStart(3, "0");
+  const result = fs.readFileSync(
+    `${resultsSamplesPath}/result${fileNumber}.json`
+  );
+  return server.post("/result").send(JSON.parse(result));
 }
 
 async function loadResults(server, quantity) {
-    const results = [];
-    for (let count = 0; count < quantity; count += 1) {
-        results.push(await loadAndPostResultFile(server, count + 1));
-    }
-    return results;
+  const results = [];
+  for (let count = 0; count < quantity; count += 1) {
+    results.push(await loadAndPostResultFile(server, count + 1));
+  }
+  return results;
 }
 
 function fetchScoreboard(server) {
-    return server.get('/scoreboard');
+  return server.get("/scoreboard");
 }
 
-describe('Scoreboard Tests', () => {
-    const server = request(expressServer);
+describe("Scoreboard Tests", () => {
+  const server = request(expressServer);
 
-    beforeEach(() => {
-        resetScores();
-    });
+  beforeEach(() => {
+    resetScores();
+  });
 
-    test('first 5', async () => {
-        await loadResults(server, 5);
-        const scoreboard = await fetchScoreboard(server);
-        expect(scoreboard).not.toBeNull();
-        // assert LD == 1
-        // assert LAB = 4
-        // assert winner = noone
-    });
+  test("first 5", async () => {
+    await loadResults(server, 5);
+    const scoreboard = await fetchScoreboard(server);
+    expect(scoreboard).not.toBeNull();
+    const response = scoreboard.body;
+    expect(response.winner).toBe(null);
+    expect(response.LD.seats).toBe(1);
+    expect(response.LAB.seats).toBe(4);
+  });
 
-    test('first 100', async () => {
-        await loadResults(server, 100);
-        const scoreboard = await fetchScoreboard(server);
-        expect(scoreboard).not.toBeNull();
-        // assert LD == 12
-        // assert LAB == 56
-        // assert CON == 31
-        // assert SGP == 0
-        // assert winner = noone
-        // Bonus Task (total votes):
-        // assert SGP == 1071
-    });
+  test("first 100", async () => {
+    await loadResults(server, 100);
+    const scoreboard = await fetchScoreboard(server);
+    expect(scoreboard).not.toBeNull();
+    const response = scoreboard.body;
 
-    test('first 554', async () => {
-        await loadResults(server, 554);
-        const scoreboard = await fetchScoreboard(server);
-        expect(scoreboard).not.toBeNull();
-        // assert LD == 52
-        // assert LAB = 325
-        // assert CON = 167
-        // assert IKHH = 1
-        // assert winner = LAB
-        // Bonus Task (total votes):
-        // assert IKHH == 18739
-    });
+    // assert winner = noone
+    expect(response.winner).toBe(null);
+    // assert LD == 12
+    expect(response.LD.seats).toBe(12);
+    // assert LAB == 56
+    expect(response.LAB.seats).toBe(56);
+    // assert CON == 31
+    expect(response.CON.seats).toBe(31);
+    // assert SGP == 0
+    expect(response.SGP.seats).toBe(0);
+    // Bonus Task (total votes):
+    // assert SGP == 1071
+    expect(response.SGP.voteShare).toBe(1071);
+  });
 
-    test('test all results', async () => {
-        await loadResults(server, 650);
-        const scoreboard = await fetchScoreboard(server);
-        expect(scoreboard).not.toBeNull();
-        // assert LD == 62
-        // assert LAB == 349
-        // assert CON == 210
-        // assert SDLP == 3
-        // assert winner = LAB
-        // assert sum = 650
-        // Bonus Task (total votes):
-        // assert SDLP == 125626
-    });
+  test("first 554", async () => {
+    await loadResults(server, 554);
+    const scoreboard = await fetchScoreboard(server);
+    expect(scoreboard).not.toBeNull();
+    const response = scoreboard.body;
+    // assert LD == 52
+    expect(response.LD.seats).toBe(52);
+    // assert LAB = 325
+    expect(response.LAB.seats).toBe(325);
+    // assert CON = 167
+    expect(response.CON.seats).toBe(167);
+    // assert IKHH = 1
+    expect(response.IKHH.seats).toBe(1);
+    // assert winner = LAB
+    expect(response.winner).toBe("LAB");
+    // Bonus Task (total votes):
+    // assert IKHH == 18739
+    expect(response.IKHH.voteShare).toBe(18739);
+  });
+
+  test("test all results", async () => {
+    await loadResults(server, 650);
+    const scoreboard = await fetchScoreboard(server);
+    expect(scoreboard).not.toBeNull();
+    const response = scoreboard.body;
+    // assert LD == 62
+    expect(response.LD.seats).toBe(62);
+    // assert LAB == 349
+    expect(response.LAB.seats).toBe(349);
+    // assert CON == 210
+    expect(response.CON.seats).toBe(210);
+    // assert SDLP == 3
+    expect(response.SDLP.seats).toBe(3);
+    // assert winner = LAB
+    expect(response.winner).toBe("LAB");
+    // assert sum = 650
+    expect(response.sum).toBe(650);
+    // Bonus Task (total votes):
+    // assert SDLP == 125626
+    expect(response.SDLP.voteShare).toBe(125626);
+  });
 });
